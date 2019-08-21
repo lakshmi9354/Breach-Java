c0e697236238b7aca8a259e88f1e1590eb988009
sharathgs <sharathgs777@gmail.com>
2019-08-21 18:43:51 +0530

    pom changes

--- src/main/java/com/ing/breachmanagement/controller/ReopenController.java
+++ src/main/java/com/ing/breachmanagement/controller/ReopenController.java
@@ -1,7 +1,5 @@
 package com.ing.breachmanagement.controller;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
---
+++ src/test/java/com/ing/breachmanagement/service/BreachTicketServiceTest.java
@@ -0,0 +1,19 @@
+package com.ing.breachmanagement.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BreachTicketServiceTest {
+
+	@Test
+	public void testGetReopenTicket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBreachAction() {
+		fail("Not yet implemented");
+	}
+
+}
