e5a8f51030dcf08700e3e8f65c52e86fd81cb336
lakshmi9354 <mplnarasimham@gmail.com>
2019-08-21 18:30:17 +0530

    by saicharan

---
+++ src/main/java/com/ing/breachmanagement/dto/RiskProfileDto.java
@@ -0,0 +1,13 @@
+package com.ing.breachmanagement.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Setter
+@Getter
+@ToString
+public class RiskProfileDto {
+
+}
